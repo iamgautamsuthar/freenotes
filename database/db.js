19476457db64add0c5260aae5a011c2277432f31
Gautam Suthar <iamgautamsuthar@gmail.com>
2024-09-16 10:58:12 +0530

    pfp change

--- database/db.js
+++ database/db.js
@@ -1,368 +1,364 @@
-
 export const teamData = [
     {
-        name: "Gautam Suthar",
-        post: "Founder / Coder",
-        img: "/gautam-suthar.jpg"
+        name: 'Gautam Suthar',
+        post: 'Founder / Coder',
+        img: '/gautam-suthar.png',
     },
     {
-        name: "Ishika Patel",
-        post: "Founder / Material Provider",
-        img: "/ishika-patel.jpg"
+        name: 'Ishika Patel',
+        post: 'Founder / Material Provider',
+        img: '/ishika-patel.jpg',
     },
-]
+];
 
 export const sem = [
     {
-        text: "Sem - 1",
-        link: "/home/sem/1",
+        text: 'Sem - 1',
+        link: '/home/sem/1',
     },
     {
-        text: "Sem - 2",
-        link: "/home/sem/2",
+        text: 'Sem - 2',
+        link: '/home/sem/2',
     },
     {
-        text: "Sem - 3",
-        link: "/home/sem/3",
+        text: 'Sem - 3',
+        link: '/home/sem/3',
     },
     {
-        text: "Sem - 4",
-        link: "/home/sem/4",
+        text: 'Sem - 4',
+        link: '/home/sem/4',
     },
     {
-        text: "Sem - 5",
-        link: "/home/sem/5",
+        text: 'Sem - 5',
+        link: '/home/sem/5',
     },
     {
-        text: "Sem - 6",
-        link: "/home/sem/6",
+        text: 'Sem - 6',
+        link: '/home/sem/6',
     },
     {
-        text: "Sem - 7",
-        link: "/home/sem/7",
+        text: 'Sem - 7',
+        link: '/home/sem/7',
     },
     {
-        text: "Sem - 8",
-        link: "/home/sem/8",
+        text: 'Sem - 8',
+        link: '/home/sem/8',
     },
-]
+];
 
 export const subject = {
     sem1: [
         {
-            text: "I.C",
-            link: "/home/sem/1/ic",
-            urlName: "ic",
+            text: 'I.C',
+            link: '/home/sem/1/ic',
+            urlName: 'ic',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "www.google.com"
+                    text: 'Unit 01',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "DPA",
-            link: "/home/sem/1/dpa",
-            urlName: "dpa",
+            text: 'DPA',
+            link: '/home/sem/1/dpa',
+            urlName: 'dpa',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "www.google.com"
+                    text: 'Unit 01',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
-
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "Statistics",
-            link: "/home/sem/1/stats-1",
-            urlName: "stats-1",
+            text: 'Statistics',
+            link: '/home/sem/1/stats-1',
+            urlName: 'stats-1',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "www.google.com"
+                    text: 'Unit 01',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "CPPM",
-            link: "/home/sem/1/cppm",
-            urlName: "cppm",
+            text: 'CPPM',
+            link: '/home/sem/1/cppm',
+            urlName: 'cppm',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "/assets/sem-1/cppm/CPP01.pdf"
+                    text: 'Unit 01',
+                    link: '/assets/sem-1/cppm/CPP01.pdf',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "C.S",
-            link: "/home/sem/1/cs",
-            urlName: "cs",
+            text: 'C.S',
+            link: '/home/sem/1/cs',
+            urlName: 'cs',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "www.google.com"
+                    text: 'Unit 01',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "BKS",
-            link: "/home/sem/1/bks-1",
-            urlName: "bks-1",
+            text: 'BKS',
+            link: '/home/sem/1/bks-1',
+            urlName: 'bks-1',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "www.google.com"
+                    text: 'Unit 01',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "Canva / Coral",
-            link: "/home/sem/1/canva-coral",
-            urlName: "canva-coral",
+            text: 'Canva / Coral',
+            link: '/home/sem/1/canva-coral',
+            urlName: 'canva-coral',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "www.google.com"
+                    text: 'Unit 01',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
     ],
     sem2: [
         {
-            text: "RDBMS",
-            link: "/home/sem/2/rdbms",
-            urlName: "rdbms",
+            text: 'RDBMS',
+            link: '/home/sem/2/rdbms',
+            urlName: 'rdbms',
         },
         {
-            text: "BKS",
-            link: "/home/sem/2/bks-2",
-            urlName: "bks",
-
+            text: 'BKS',
+            link: '/home/sem/2/bks-2',
+            urlName: 'bks',
         },
         {
-            text: "OC",
-            link: "/home/sem/2/oc",
-            urlName: "oc",
+            text: 'OC',
+            link: '/home/sem/2/oc',
+            urlName: 'oc',
         },
         {
-            text: "Programming Skills",
-            link: "/home/sem/2/programming-skills",
-            urlName: "programming-skills",
+            text: 'Programming Skills',
+            link: '/home/sem/2/programming-skills',
+            urlName: 'programming-skills',
         },
         {
-            text: "Wordpress",
-            link: "/home/sem/2/wordpress",
-            urlName: "wordpress",
+            text: 'Wordpress',
+            link: '/home/sem/2/wordpress',
+            urlName: 'wordpress',
         },
         {
-            text: "Operating System",
-            link: "/home/sem/2/operating-system",
-            urlName: "operating system",
+            text: 'Operating System',
+            link: '/home/sem/2/operating-system',
+            urlName: 'operating system',
         },
         {
-            text: "Soft Skills",
-            link: "/home/sem/2/soft-skills",
-            urlName: "soft-skills",
+            text: 'Soft Skills',
+            link: '/home/sem/2/soft-skills',
+            urlName: 'soft-skills',
         },
     ],
     sem3: [
         {
-            text: "OOPS",
-            link: "/home/sem/3/oops",
-            urlName: "oops",
+            text: 'OOPS',
+            link: '/home/sem/3/oops',
+            urlName: 'oops',
         },
         {
-            text: "Python / Sqlite",
-            link: "/home/sem/3/python-sqlite",
-            urlName: "python-sqlite",
+            text: 'Python / Sqlite',
+            link: '/home/sem/3/python-sqlite',
+            urlName: 'python-sqlite',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "/assets/sem-3/python-sqlite/python-01.pdf"
+                    text: 'Unit 01',
+                    link: '/assets/sem-3/python-sqlite/python-01.pdf',
                 },
                 {
-                    text: "Unit 02",
-                    link: "/assets/sem-3/python-sqlite/python-02.pdf"
+                    text: 'Unit 02',
+                    link: '/assets/sem-3/python-sqlite/python-02.pdf',
                 },
                 {
-                    text: "Unit 03",
-                    link: "/assets/sem-3/python-sqlite/python-03.pdf"
+                    text: 'Unit 03',
+                    link: '/assets/sem-3/python-sqlite/python-03.pdf',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "Web - 1",
-            link: "/home/sem/3/web-1",
-            urlName: "web-1",
+            text: 'Web - 1',
+            link: '/home/sem/3/web-1',
+            urlName: 'web-1',
         },
         {
-            text: "Android - 1",
-            link: "/home/sem/3/android-1",
-            urlName: "android-1",
+            text: 'Android - 1',
+            link: '/home/sem/3/android-1',
+            urlName: 'android-1',
         },
         {
-            text: "Statistics",
-            link: "/home/sem/3/stats-2",
-            urlName: "stats-2",
+            text: 'Statistics',
+            link: '/home/sem/3/stats-2',
+            urlName: 'stats-2',
         },
         {
-            text: "R studio",
-            link: "/home/sem/3/r-studio",
-            urlName: "r-studio",
+            text: 'R studio',
+            link: '/home/sem/3/r-studio',
+            urlName: 'r-studio',
         },
         {
-            text: "BKS",
-            link: "/home/sem/3/bks-3",
-            urlName: "bks-3",
+            text: 'BKS',
+            link: '/home/sem/3/bks-3',
+            urlName: 'bks-3',
             pdf: [
                 {
-                    text: "Unit 01",
-                    link: "/assets/sem-3/bks/bks-01.pdf"
+                    text: 'Unit 01',
+                    link: '/assets/sem-3/bks/bks-01.pdf',
                 },
                 {
-                    text: "Unit 02",
-                    link: "www.google.com"
+                    text: 'Unit 02',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 03",
-                    link: "www.google.com"
+                    text: 'Unit 03',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 04",
-                    link: "www.google.com"
+                    text: 'Unit 04',
+                    link: 'www.google.com',
                 },
                 {
-                    text: "Unit 05",
-                    link: "www.google.com"
-                }
-            ]
+                    text: 'Unit 05',
+                    link: 'www.google.com',
+                },
+            ],
         },
         {
-            text: "MIL",
-            link: "/home/sem/3/mil",
-            urlName: "mil",
+            text: 'MIL',
+            link: '/home/sem/3/mil',
+            urlName: 'mil',
         },
-    ]
-}
-
+    ],
+};
